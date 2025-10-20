acb5d0b69939a3b36a5f74a20ef2c17a60d54d6c
Elbert <elbertwithane@gmail.com>
2025-10-20 20:04:50 +0800

    finished home header

--- frontend/src/commonComponents/HeaderLogo.tsx
+++ frontend/src/commonComponents/HeaderLogo.tsx
@@ -1,7 +1,39 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function HeaderLogo() {
+    const [isOpen, setIsOpen] = useState(false);
+    const nav = useNavigate();
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="root-logo-header">
-            <div>So When</div>
+            <div onClick={handleToggle} style={{ cursor: "pointer" }}>
+                So When
+            </div>
+            {isOpen && (
+                <div className="dropdown-menu">
+                    <div
+                        className="dropdown-item"
+                        onClick={() => {
+                            nav("/home");
+                        }}
+                    >
+                        Home
+                    </div>
+                    <div
+                        className="dropdown-item"
+                        onClick={() => {
+                            nav("/new");
+                        }}
+                    >
+                        Create new event
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
